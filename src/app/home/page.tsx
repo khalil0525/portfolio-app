3712e6d20d1bfb037dbe389f669ea403f1808c1d
Khalil Collins <Collinskhalil@hotmail.com>
2023-12-08 10:57:16 -0500

    Updated home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,102 +1,120 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { Box, Text, Flex } from "@chakra-ui/react";
-import { ThreeCircles } from "react-loader-spinner";
-import { useSpring, animated, config } from "react-spring";
-import Image from "next/image";
+'use client';
+import React, { useEffect, useState } from 'react';
+import { Box, Text, Flex } from '@chakra-ui/react';
+import { ThreeCircles } from 'react-loader-spinner';
+import { useSpring, animated, config } from 'react-spring';
+import Image from 'next/image';
+
 export default function Page() {
-	const [isPageLoaded, setIsPageLoaded] = useState(false);
+  const [isPageLoaded, setIsPageLoaded] = useState(false);
 
-	useEffect(() => {
-		// Simulate page loading with a delay (you can remove this in production)
-		const loadingTimer = setTimeout(() => {
-			setIsPageLoaded(true);
-		}, 500); // Adjust the time as needed
+  useEffect(() => {
+    // Simulate page loading with a delay (you can remove this in production)
+    const loadingTimer = setTimeout(() => {
+      setIsPageLoaded(true);
+    }, 500); // Adjust the time as needed
 
-		// Clear the timer when the component unmounts
-		return () => clearTimeout(loadingTimer);
-	}, []);
+    // Clear the timer when the component unmounts
+    return () => clearTimeout(loadingTimer);
+  }, []);
 
-	// Define the animation config for the text
-	const textAnimation = useSpring({
-		opacity: isPageLoaded ? 1 : 0,
-		transform: isPageLoaded ? "translateY(0)" : "translateY(20px)",
-		config: {
-			tension: 5000, // Adjust the animation tension
-			friction: 500, // Adjust the animation friction
-		},
-	});
+  // Define the animation config for the text
+  const textAnimation = useSpring({
+    opacity: isPageLoaded ? 1 : 0,
+    transform: isPageLoaded ? 'translateY(0)' : 'translateY(20px)',
+    config: {
+      tension: 5000, // Adjust the animation tension
+      friction: 500, // Adjust the animation friction
+    },
+  });
 
-	// Define the animation config for the image
-	const imageAnimation = useSpring({
-		opacity: isPageLoaded ? 1 : 0,
-		transform: isPageLoaded ? "translateY(0)" : "translateY(20px)",
-		config: config.gentle, // Use a gentle animation
-		delay: 50, // Delay the animation to start after text animation
-	});
+  // Define the animation config for the image
+  const imageAnimation = useSpring({
+    opacity: isPageLoaded ? 1 : 0,
+    transform: isPageLoaded ? 'translateY(0)' : 'translateY(20px)',
+    config: config.gentle, // Use a gentle animation
+    delay: 50, // Delay the animation to start after text animation
+  });
 
-	return (
-		<>
-			{!isPageLoaded ? (
-				// Show the loader until the page is loaded
-				<Box
-					w="100%"
-					h="100vh"
-					display="flex"
-					alignItems="center"
-					justifyContent="center"
-				>
-					<ThreeCircles color="#1CFF25" height={80} width={80} />
-				</Box>
-			) : (
-				<>
-					<animated.div style={textAnimation}>
-						<Text
-							as="h1"
-							size="3xl"
-							color="#C1CED9"
-							fontSize={["24px", "48px", "48px", "64px"]}
-							fontWeight="bold"
-							letterSpacing="4px"
-							mb="16px"
-						>
-							Khalil.
-							<span style={{ color: "#1CFF25" }}>Collins()</span>
-						</Text>
-					</animated.div>
-					<Flex
-						direction={["column", "column", "column", "row"]}
-						w="100%"
-						justify={["unset", "unset", "unset", "space-evenly"]}
-					>
-						<Box
-							position="relative" // Add this line to create a relative container for the image
-							w={["100%", "100%", "100%", "55%"]}
-							h={["40vh", "40vh", "40vh", "80%"]}
-							mb={["12px", "12px", 0]}
-							flexShrink={1}
-						>
-							<animated.div
-								style={{
-									...imageAnimation,
-									display: "flex",
-									justifyContent: "center",
-									width: "100%",
-									height: "100%",
-								}}
-							>
-								<Image
-									src="/img/logo/green-logo-min.png"
-									alt="Logo"
-									width={800}
-									height={800}
-									loading="eager"
-								/>
-							</animated.div>
-						</Box>
-					</Flex>
-				</>
-			)}
-		</>
-	);
+  return (
+    <>
+      {!isPageLoaded ? (
+        // Show the loader until the page is loaded
+        <Box
+          w="100%"
+          h="100vh"
+          display="flex"
+          alignItems="center"
+          justifyContent="center">
+          <ThreeCircles
+            color="#1CFF25"
+            height={80}
+            width={80}
+          />
+        </Box>
+      ) : (
+        <>
+          <animated.div style={textAnimation}>
+            <Text
+              as="h1"
+              size="3xl"
+              color="#C1CED9"
+              fontSize={['24px', '48px', '48px', '64px']}
+              fontWeight="bold"
+              letterSpacing="4px"
+              mb="16px">
+              Khalil.
+              <span style={{ color: '#1CFF25' }}>Collins()</span>
+            </Text>
+          </animated.div>
+          <Text
+            as="h2"
+            fontSize="16px"
+            fontWeight="500"
+            letterSpacing="2px"
+            textAlign="center"
+            mb="16px"
+            width="70%">
+            <span style={{ color: '#1CFF25' }}>Full Stack Engineer</span> with a
+            passion for <span style={{ color: '#FFD700' }}>designing</span> and
+            building web apps. Turning ideas into{' '}
+            <span style={{ color: '#FF1493' }}>code</span> is my thrill—where{' '}
+            <span style={{ color: '#4CAF50' }}>creativity</span> meets
+            functionality, and each line of{' '}
+            <span style={{ color: '#FF5722' }}>code</span> breathes life into{' '}
+            <span style={{ color: '#9C27B0' }}>digital</span> visions.
+          </Text>
+
+          <Flex
+            direction={['column', 'column', 'column', 'row']}
+            w="100%"
+            justify={['unset', 'unset', 'unset', 'space-evenly']}>
+            <Box
+              position="relative" // Add this line to create a relative container for the image
+              w={['100%', '100%', '100%', '55%']}
+              h={['40vh', '40vh', '40vh', '80%']}
+              mb={['12px', '12px', 0]}
+              flexShrink={1}>
+              <animated.div
+                style={{
+                  ...imageAnimation,
+                  display: 'flex',
+                  justifyContent: 'center',
+                  width: '100%',
+                  height: '100%',
+                }}>
+                <Image
+                  src="/img/logo/green-logo-min.png"
+                  alt="Logo"
+                  width={800}
+                  height={800}
+                  loading="eager"
+                />
+              </animated.div>
+            </Box>
+          </Flex>
+        </>
+      )}
+    </>
+  );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,161 +1,159 @@
-"use client";
-import React, { useState } from "react";
+'use client';
+import React, { useState } from 'react';
 import {
-	Flex,
-	Button,
-	useMediaQuery,
-	Drawer,
-	DrawerOverlay,
-	DrawerContent,
-	DrawerCloseButton,
-	DrawerHeader,
-	DrawerBody,
-	Stack,
-	Box,
-} from "@chakra-ui/react";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+  Flex,
+  Button,
+  useMediaQuery,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  Stack,
+  Box,
+} from '@chakra-ui/react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 export default function NavBar() {
-	const pathName = usePathname();
-	console.log(pathName);
-	const isStartPage = pathName === "/";
-	let page = pathName.slice(1);
-	const [isLargerThan768] = useMediaQuery("(min-width: 840px)");
-	const [isDrawerOpen, setDrawerOpen] = useState(false);
-	const LinkContainerStyles = {
-		display: "inline-block",
-		paddingBottom: "10px", // Adjust the value to control the space between the Link text and the underline
-	};
-	const selectedPageLinkStyles = {
-		color: "#fff",
-		backgroundImage: "linear-gradient(to right, #1EF828, #1EF828)",
-		backgroundSize: "100% 2px",
-		backgroundRepeat: "no-repeat",
-		backgroundPosition: "0 100%",
-	};
-	const LinkStyles = {
-		background: "none",
-		color: "#fff",
-		fontSize: isLargerThan768 ? "18px" : "12px",
-		textShadow: "0 4px 4px rgba(0, 0, 0, 0.25)",
-		fontWeight: "500",
-		fontStretch: "normal",
+  const pathName = usePathname();
+  console.log(pathName);
+  const isStartPage = pathName === '/';
+  let page = pathName.slice(1);
+  const [isLargerThan768] = useMediaQuery('(min-width: 840px)');
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
+  const LinkContainerStyles = {
+    display: 'inline-block',
+    paddingBottom: '10px', // Adjust the value to control the space between the Link text and the underline
+  };
+  const selectedPageLinkStyles = {
+    color: '#fff',
+    backgroundImage: 'linear-gradient(to right, #1EF828, #1EF828)',
+    backgroundSize: '100% 2px',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: '0 100%',
+  };
+  const LinkStyles = {
+    background: 'none',
+    color: '#fff',
+    fontSize: isLargerThan768 ? '18px' : '12px',
+    textShadow: '0 4px 4px rgba(0, 0, 0, 0.25)',
+    fontWeight: '500',
+    fontStretch: 'normal',
 
-		letterSpacing: "-10%",
-	};
+    letterSpacing: '-10%',
+  };
 
-	const toggleDrawer = () => {
-		setDrawerOpen(!isDrawerOpen);
-	};
+  const toggleDrawer = () => {
+    setDrawerOpen(!isDrawerOpen);
+  };
 
-	const handleNavItemClick = () => {
-		setDrawerOpen(false); // Close the drawer on mobile when a link is clicked
-	};
+  const handleNavItemClick = () => {
+    setDrawerOpen(false); // Close the drawer on mobile when a link is clicked
+  };
 
-	return (
-		<Flex
-			direction="row"
-			p={["6px", "6px", "16px", "24px", "36px"]}
-			gap={["6px", "6px", "36px", "36px"]}
-			justify="space-between"
-			alignSelf="end"
-			position={isLargerThan768 ? "static" : "sticky"} // Set position to sticky on mobile
-			top={0} // Stick to the top of the viewport on mobile
-			zIndex={999} // Set a higher z-index to ensure it's above other elements
-		>
-			{/* Regular Navigation Links */}
-			{isLargerThan768 ? (
-				<>
-					<Link
-						href="/home"
-						style={page === "home" ? selectedPageLinkStyles : {}}
-					>
-						home
-					</Link>
-					<Link
-						href="/about"
-						style={page === "about" ? selectedPageLinkStyles : {}}
-					>
-						about
-					</Link>
-					<Link
-						href="/skills"
-						style={page === "skills" ? selectedPageLinkStyles : {}}
-					>
-						skills
-					</Link>
-					<Link
-						href="/projects"
-						style={page === "projects" ? selectedPageLinkStyles : {}}
-					>
-						my projects
-					</Link>
-					<Link
-						href="/contact"
-						style={page === "contact" ? selectedPageLinkStyles : {}}
-					>
-						contact
-					</Link>
-				</>
-			) : (
-				<Flex direction="row" gap="16px" align={"end"}>
-					{/* Mobile Menu Link */}
-					<Button style={LinkStyles} onClick={toggleDrawer}>
-						☰
-					</Button>
-				</Flex>
-			)}
+  return (
+    <Flex
+      direction="row"
+      p={['6px', '6px', '16px', '24px', '36px']}
+      gap={['6px', '6px', '36px', '36px']}
+      justify="space-between"
+      alignSelf="middle"
+      position={isLargerThan768 ? 'static' : 'sticky'} // Set position to sticky on mobile
+      top={0} // Stick to the top of the viewport on mobile
+      zIndex={999} // Set a higher z-index to ensure it's above other elements
+    >
+      {/* Regular Navigation Links */}
+      {isLargerThan768 ? (
+        <>
+          <Link
+            href="/home"
+            style={page === 'home' ? selectedPageLinkStyles : {}}>
+            home
+          </Link>
+          <Link
+            href="/about"
+            style={page === 'about' ? selectedPageLinkStyles : {}}>
+            about
+          </Link>
+          <Link
+            href="/skills"
+            style={page === 'skills' ? selectedPageLinkStyles : {}}>
+            skills
+          </Link>
+          <Link
+            href="/projects"
+            style={page === 'projects' ? selectedPageLinkStyles : {}}>
+            my projects
+          </Link>
+          <Link
+            href="/contact"
+            style={page === 'contact' ? selectedPageLinkStyles : {}}>
+            contact
+          </Link>
+        </>
+      ) : (
+        <Flex
+          direction="row"
+          gap="16px"
+          align={'end'}>
+          {/* Mobile Menu Link */}
+          <Button
+            style={LinkStyles}
+            onClick={toggleDrawer}>
+            ☰
+          </Button>
+        </Flex>
+      )}
 
-			{/* Mobile Drawer */}
-			<Drawer isOpen={isDrawerOpen} placement="right" onClose={toggleDrawer}>
-				<DrawerOverlay>
-					<DrawerContent bg="#000">
-						{" "}
-						{/* Add the desired background color here */}
-						<DrawerCloseButton />
-						<DrawerHeader>Menu</DrawerHeader>
-						<DrawerBody>
-							<Stack spacing="8px">
-								<Link
-									href="/home"
-									style={page === "home" ? selectedPageLinkStyles : {}}
-									onClick={() => handleNavItemClick()}
-								>
-									home
-								</Link>
-								<Link
-									href="/about"
-									style={page === "about" ? selectedPageLinkStyles : {}}
-									onClick={() => handleNavItemClick()}
-								>
-									about
-								</Link>
-								<Link
-									href="/skills"
-									style={page === "skills" ? selectedPageLinkStyles : {}}
-									onClick={() => handleNavItemClick()}
-								>
-									skills
-								</Link>
-								<Link
-									href="/projects"
-									style={page === "projects" ? selectedPageLinkStyles : {}}
-									onClick={() => handleNavItemClick()}
-								>
-									my projects
-								</Link>
-								<Link
-									href="/contact"
-									style={page === "contact" ? selectedPageLinkStyles : {}}
-									onClick={() => handleNavItemClick()}
-								>
-									contact
-								</Link>
-							</Stack>
-						</DrawerBody>
-					</DrawerContent>
-				</DrawerOverlay>
-			</Drawer>
-		</Flex>
-	);
+      {/* Mobile Drawer */}
+      <Drawer
+        isOpen={isDrawerOpen}
+        placement="right"
+        onClose={toggleDrawer}>
+        <DrawerOverlay>
+          <DrawerContent bg="#000">
+            {' '}
+            {/* Add the desired background color here */}
+            <DrawerCloseButton />
+            <DrawerHeader>Menu</DrawerHeader>
+            <DrawerBody>
+              <Stack spacing="8px">
+                <Link
+                  href="/home"
+                  style={page === 'home' ? selectedPageLinkStyles : {}}
+                  onClick={() => handleNavItemClick()}>
+                  home
+                </Link>
+                <Link
+                  href="/about"
+                  style={page === 'about' ? selectedPageLinkStyles : {}}
+                  onClick={() => handleNavItemClick()}>
+                  about
+                </Link>
+                <Link
+                  href="/skills"
+                  style={page === 'skills' ? selectedPageLinkStyles : {}}
+                  onClick={() => handleNavItemClick()}>
+                  skills
+                </Link>
+                <Link
+                  href="/projects"
+                  style={page === 'projects' ? selectedPageLinkStyles : {}}
+                  onClick={() => handleNavItemClick()}>
+                  my projects
+                </Link>
+                <Link
+                  href="/contact"
+                  style={page === 'contact' ? selectedPageLinkStyles : {}}
+                  onClick={() => handleNavItemClick()}>
+                  contact
+                </Link>
+              </Stack>
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </Flex>
+  );
 }
