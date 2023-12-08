87c8e0976a3c3b476908c366202d167ef26afed8
Khalil Collins <Collinskhalil@hotmail.com>
2023-12-08 14:55:46 -0500

    Changes to skills

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,126 +1,127 @@
-"use client";
+'use client';
 
-import React, { useEffect, useState } from "react";
-import { Image, Text, Stack, Flex, Box } from "@chakra-ui/react";
-import { motion } from "framer-motion";
-import { ThreeCircles } from "react-loader-spinner";
+import React, { useEffect, useState } from 'react';
+import { Image, Text, Stack, Flex, Box } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
+import { ThreeCircles } from 'react-loader-spinner';
 export default function Page() {
-	const [isTextVisible, setIsTextVisible] = useState(false);
-	const [isPageLoaded, setIsPageLoaded] = useState(false);
+  const [isTextVisible, setIsTextVisible] = useState(false);
+  const [isPageLoaded, setIsPageLoaded] = useState(false);
 
-	useEffect(() => {
-		// Simulate page loading with a delay (you can remove this in production)
-		const loadingTimer = setTimeout(() => {
-			setIsPageLoaded(true);
-		}, 500); // Adjust the time as needed
+  useEffect(() => {
+    // Simulate page loading with a delay (you can remove this in production)
+    const loadingTimer = setTimeout(() => {
+      setIsPageLoaded(true);
+    }, 500); // Adjust the time as needed
 
-		// Clear the timer when the component unmounts
-		return () => clearTimeout(loadingTimer);
-	}, []);
+    // Clear the timer when the component unmounts
+    return () => clearTimeout(loadingTimer);
+  }, []);
 
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			setIsTextVisible(true);
-		}, 1000); // 3000 milliseconds delay (3 seconds)
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsTextVisible(true);
+    }, 1000); // 3000 milliseconds delay (3 seconds)
 
-		return () => clearTimeout(timer);
-	}, []);
+    return () => clearTimeout(timer);
+  }, []);
 
-	return (
-		<>
-			{!isPageLoaded ? (
-				// Show the loader until the page is loaded
-				<Box
-					w="100%"
-					h="100vh"
-					display="flex"
-					alignItems="center"
-					justifyContent="center"
-				>
-					<ThreeCircles color="#1CFF25" height={80} width={80} />
-				</Box>
-			) : (
-				<>
-					<Flex
-						direction={["column", "column", "column", "row"]}
-						w="100%"
-						justify={["unset", "unset", "unset", "space-evenly"]}
-						overflowY={["unset", "unset", "unset", "hidden"]}
-					>
-						<Flex
-							direction="column"
-							p="8px"
-							w={["100%", "100%", "100%", "55%"]}
-							h={["40vh", "40vh", "40vh", "80%"]}
-							mb={["12px", "12px", 0]}
-							flexShrink={1}
-						>
-							<Image
-								src="/img/logo/green-logo-min.png"
-								w="100%" // Add this to ensure the image remains its original size
-								h={["100%", "100%", "100%"]}
-							/>
-						</Flex>
+  return (
+    <>
+      {!isPageLoaded ? (
+        // Show the loader until the page is loaded
+        <Box
+          w="100%"
+          h="100vh"
+          display="flex"
+          alignItems="center"
+          justifyContent="center">
+          <ThreeCircles
+            color="#1CFF25"
+            height={80}
+            width={80}
+          />
+        </Box>
+      ) : (
+        <>
+          <Flex
+            direction={['column', 'column', 'column', 'row']}
+            w="100%"
+            justify={['unset', 'unset', 'unset', 'space-evenly']}
+            overflowY={['unset', 'unset', 'unset', 'hidden']}>
+            <Flex
+              direction="column"
+              p="8px"
+              w={['100%', '100%', '100%', '55%']}
+              h={['40vh', '40vh', '40vh', '80%']}
+              mb={['12px', '12px', 0]}
+              flexShrink={1}>
+              <Image
+                src="/img/logo/green-logo-min.png"
+                w="100%"
+                h={['100%', '100%', '100%']}
+                alt="khalil collins logo"
+              />
+            </Flex>
 
-						<Stack
-							spacing={6}
-							fontSize="22px"
-							fontWeight="300"
-							maxW={["100%", "100%", "100%", "35%"]}
-							w={["100%", "100%", "100%", "35%"]}
-							justifySelf="end"
-							overflowY={["auto", "auto", "auto", "auto"]}
-							h={["25vh", "30vh", "30vh", "50%"]}
-							className="custom-scrollbar vertical"
-							padding="36px"
-						>
-							<motion.div
-								initial={{ opacity: 0 }}
-								animate={{ opacity: isTextVisible ? 1 : 0 }}
-								transition={{ duration: 2.5 }} // Adjust the duration for the desired fade-in time
-							>
-								<Text>
-									Hey there, I&apos;m Khalil Collins, and I&apos;m thrilled to
-									have you explore my journey in the world of technology and web
-									development. 🌐🎮{" "}
-								</Text>
-								<Text>
-									I&apos;ve been immersed in the IT realm for several years,
-									honing my skills and gaining valuable experiences along the
-									way. Initially, my passion for computer science led me down
-									the path of game development, eager to create captivating
-									virtual worlds. 🎮 However, fate had a different plan for me,
-									and I stumbled upon web development, where I found my true
-									calling and fell head over heels in love with problem-solving.
-									💻❤️
-								</Text>
-								<Text>
-									Being a full-stack web developer allows me to combine my
-									creativity with my analytical mindset, crafting innovative
-									solutions that address the specific needs of various
-									industries. 🚀 It&apos;s truly fulfilling to navigate through
-									diverse challenges and provide tailor-made coding solutions
-									that make a tangible impact. 💼🔧
-								</Text>
-								<Text>
-									Through my portfolio, you&apos;ll get a glimpse into the
-									projects I&apos;ve worked on, the technologies I&apos;ve
-									mastered, and the passion I bring to every endeavor. I&apos;m
-									excited to share this digital space with you and demonstrate
-									my dedication to creating seamless user experiences and
-									scalable solutions. 🌟
-								</Text>
-								<Text>
-									Thank you for visiting, and I&apos;m looking forward to
-									connecting with fellow enthusiasts, collaborators, and
-									potential clients. Let&apos;s embark on this coding adventure
-									together! 🚀🌐
-								</Text>
-							</motion.div>
-						</Stack>
-					</Flex>
-				</>
-			)}
-		</>
-	);
+            <Stack
+              spacing={6}
+              fontSize="22px"
+              fontWeight="300"
+              maxW={['100%', '100%', '100%', '35%']}
+              w={['100%', '100%', '100%', '35%']}
+              justifySelf="end"
+              overflowY={['auto', 'auto', 'auto', 'auto']}
+              h={['25vh', '30vh', '30vh', '50%']}
+              className="custom-scrollbar vertical"
+              padding="36px">
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: isTextVisible ? 1 : 0 }}
+                transition={{ duration: 2.5 }} // Adjust the duration for the desired fade-in time
+              >
+                <Text>
+                  Hey there, I&apos;m Khalil Collins, and I&apos;m thrilled to
+                  have you explore my journey in the world of technology and web
+                  development. 🌐🎮{' '}
+                </Text>
+                <Text>
+                  I&apos;ve been immersed in the IT realm for several years,
+                  honing my skills and gaining valuable experiences along the
+                  way. Initially, my passion for computer science led me down
+                  the path of game development, eager to create captivating
+                  virtual worlds. 🎮 However, fate had a different plan for me,
+                  and I stumbled upon web development, where I found my true
+                  calling and fell head over heels in love with problem-solving.
+                  💻❤️
+                </Text>
+                <Text>
+                  Being a full-stack web developer allows me to combine my
+                  creativity with my analytical mindset, crafting innovative
+                  solutions that address the specific needs of various
+                  industries. 🚀 It&apos;s truly fulfilling to navigate through
+                  diverse challenges and provide tailor-made coding solutions
+                  that make a tangible impact. 💼🔧
+                </Text>
+                <Text>
+                  Through my portfolio, you&apos;ll get a glimpse into the
+                  projects I&apos;ve worked on, the technologies I&apos;ve
+                  mastered, and the passion I bring to every endeavor. I&apos;m
+                  excited to share this digital space with you and demonstrate
+                  my dedication to creating seamless user experiences and
+                  scalable solutions. 🌟
+                </Text>
+                <Text>
+                  Thank you for visiting, and I&apos;m looking forward to
+                  connecting with fellow enthusiasts, collaborators, and
+                  potential clients. Let&apos;s embark on this coding adventure
+                  together! 🚀🌐
+                </Text>
+              </motion.div>
+            </Stack>
+          </Flex>
+        </>
+      )}
+    </>
+  );
 }
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,116 +1,128 @@
-import React from "react";
-import { Box, Flex, Text } from "@chakra-ui/react";
-import Image from "next/image";
+import React from 'react';
+import { Box, Flex, Text, Tooltip } from '@chakra-ui/react';
+import Image from 'next/image';
 interface Skill {
-	id: number;
-	name: string;
-	imageUrl: string;
+  id: number;
+  name: string;
+  imageUrl: string;
 }
 
 const skillsData: Skill[] = [
-	{
-		id: 1,
-		name: "CSS",
-		imageUrl: "/img/logo/csslogo.png",
-	},
-	{
-		id: 2,
-		name: "JavaScript",
-		imageUrl: "/img/logo/jslogo.png",
-	},
-	{
-		id: 3,
-		name: "React",
-		imageUrl: "/img/logo/reactlogo.png",
-	},
+  {
+    id: 1,
+    name: 'CSS',
+    imageUrl: '/img/logo/csslogo.png',
+  },
+  {
+    id: 2,
+    name: 'JavaScript',
+    imageUrl: '/img/logo/jslogo.png',
+  },
+  {
+    id: 3,
+    name: 'React',
+    imageUrl: '/img/logo/reactlogo.png',
+  },
 
-	{
-		id: 4,
-		name: "HTML",
-		imageUrl: "/img/logo/htmllogo.png",
-	},
-	{
-		id: 5,
-		name: "Node js",
-		imageUrl: "/img/logo/node.png",
-	},
-	{
-		id: 6,
-		name: "Postgresql",
-		imageUrl: "/img/logo/postgresqllogo.png",
-	},
-	{
-		id: 7,
-		name: "Sequelize",
-		imageUrl: "/img/logo/sequelizelogo.png",
-	},
-	{
-		id: 8,
-		name: "Docker",
-		imageUrl: "/img/logo/docker.png",
-	},
-	{
-		id: 9,
-		name: "CI/CD",
-		imageUrl: "/img/logo/lifecyclelogo.png",
-	},
-	{
-		id: 10,
-		name: "Python",
-		imageUrl: "/img/logo/pythonlogo.png",
-	},
-	{
-		id: 11,
-		name: "Linux",
-		imageUrl: "/img/logo/linuxlogo.png",
-	},
-	{
-		id: 12,
-		name: "Typescript",
-		imageUrl: "/img/logo/typescriptlogo.png",
-	},
-	{
-		id: 13,
-		name: "Next",
-		imageUrl: "/img/logo/nextlogo.png",
-	},
+  {
+    id: 4,
+    name: 'HTML',
+    imageUrl: '/img/logo/htmllogo.png',
+  },
+  {
+    id: 5,
+    name: 'Node js',
+    imageUrl: '/img/logo/node.png',
+  },
+  {
+    id: 6,
+    name: 'Postgresql',
+    imageUrl: '/img/logo/postgresqllogo.png',
+  },
+  {
+    id: 7,
+    name: 'Sequelize',
+    imageUrl: '/img/logo/sequelizelogo.png',
+  },
+  {
+    id: 8,
+    name: 'Docker',
+    imageUrl: '/img/logo/docker.png',
+  },
+  {
+    id: 9,
+    name: 'CI/CD',
+    imageUrl: '/img/logo/lifecyclelogo.png',
+  },
+  {
+    id: 10,
+    name: 'Python',
+    imageUrl: '/img/logo/pythonlogo.png',
+  },
+  {
+    id: 11,
+    name: 'Linux',
+    imageUrl: '/img/logo/linuxlogo.png',
+  },
+  {
+    id: 12,
+    name: 'Typescript',
+    imageUrl: '/img/logo/typescriptlogo.png',
+  },
+  {
+    id: 13,
+    name: 'Next',
+    imageUrl: '/img/logo/nextlogo.png',
+  },
 
-	{
-		id: 14,
-		name: "Express",
-		imageUrl: "/img/logo/expresslogo.png",
-	},
-	// Add more skills as needed
+  {
+    id: 14,
+    name: 'Express',
+    imageUrl: '/img/logo/expresslogo.png',
+  },
+  // Add more skills as needed
 ];
 
 const Skills: React.FC = () => {
-	return (
-		<Flex direction="column" alignItems="center" width="60%" mx="auto">
-			<Flex flexWrap="wrap" justifyContent="center" alignItems="center">
-				{skillsData.map((skill) => (
-					<Box
-						key={skill.id}
-						p={5}
-						m={2}
-						width="120px"
-						height="120px"
-						borderRadius="full"
-						bg="#fff"
-						display="flex"
-						justifyContent="center"
-						alignItems="center"
-					>
-						<Image
-							src={skill.imageUrl}
-							alt={skill.name}
-							width={120}
-							height={120}
-						/>
-					</Box>
-				))}
-			</Flex>
-		</Flex>
-	);
+  return (
+    <Flex
+      direction="column"
+      alignItems="center"
+      width="80%"
+      mx="auto">
+      <Flex
+        flexWrap="wrap"
+        justifyContent="center"
+        alignItems="center">
+        {skillsData.map((skill) => (
+          <Tooltip
+            key={skill.id}
+            label={skill.name}>
+            <Box
+              p={10}
+              m={4}
+              width="160px"
+              height="160px"
+              borderRadius="full"
+              bg="#fff"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              transition="transform 0.3s ease-in-out"
+              _hover={{ transform: 'scale(1.2)' }} // Increase zoom on hover
+            >
+              <Image
+                src={skill.imageUrl}
+                alt={skill.name}
+                width={120}
+                height={120}
+              />
+            </Box>
+          </Tooltip>
+        ))}
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Skills;
