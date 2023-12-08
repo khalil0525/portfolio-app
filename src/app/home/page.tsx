fcb72892e098f889f66e059f62d2525a2fcf2086
Khalil Collins <Collinskhalil@hotmail.com>
2023-12-08 12:47:54 -0500

    Changed images

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -53,46 +53,50 @@ export default function Page() {
           />
         </Box>
       ) : (
-        <>
-          <animated.div style={textAnimation}>
+        <Flex justify="center">
+          <Flex
+            direction="column"
+            w="55%"
+            justifyContent="center">
+            <animated.div style={textAnimation}>
+              <Text
+                as="h1"
+                size="3xl"
+                color="#C1CED9"
+                fontSize={['24px', '48px', '48px', '48px']}
+                fontWeight="bold"
+                letterSpacing="4px"
+                mb="16px">
+                Khalil.
+                <span style={{ color: '#1CFF25' }}>Collins()</span>
+              </Text>
+            </animated.div>
             <Text
-              as="h1"
-              size="3xl"
-              color="#C1CED9"
-              fontSize={['24px', '48px', '48px', '64px']}
-              fontWeight="bold"
-              letterSpacing="4px"
-              mb="16px">
-              Khalil.
-              <span style={{ color: '#1CFF25' }}>Collins()</span>
+              as="h2"
+              fontSize="16px"
+              fontWeight="500"
+              letterSpacing="2px"
+              textAlign="left"
+              mb="16px"
+              width="80%">
+              <span style={{ color: '#1CFF25' }}>Full Stack Engineer</span> with
+              a passion for <span style={{ color: '#FFD700' }}>designing</span>{' '}
+              and building web apps. Turning ideas into{' '}
+              <span style={{ color: '#FF1493' }}>code</span> is my thrill—where{' '}
+              <span style={{ color: '#4CAF50' }}>creativity</span> meets
+              functionality, and each line of{' '}
+              <span style={{ color: '#FF5722' }}>code</span> breathes life into{' '}
+              <span style={{ color: '#9C27B0' }}>digital</span> visions.
             </Text>
-          </animated.div>
-          <Text
-            as="h2"
-            fontSize="16px"
-            fontWeight="500"
-            letterSpacing="2px"
-            textAlign="center"
-            mb="16px"
-            width="70%">
-            <span style={{ color: '#1CFF25' }}>Full Stack Engineer</span> with a
-            passion for <span style={{ color: '#FFD700' }}>designing</span> and
-            building web apps. Turning ideas into{' '}
-            <span style={{ color: '#FF1493' }}>code</span> is my thrill—where{' '}
-            <span style={{ color: '#4CAF50' }}>creativity</span> meets
-            functionality, and each line of{' '}
-            <span style={{ color: '#FF5722' }}>code</span> breathes life into{' '}
-            <span style={{ color: '#9C27B0' }}>digital</span> visions.
-          </Text>
-
+          </Flex>
           <Flex
             direction={['column', 'column', 'column', 'row']}
             w="100%"
             justify={['unset', 'unset', 'unset', 'space-evenly']}>
             <Box
               position="relative" // Add this line to create a relative container for the image
-              w={['100%', '100%', '100%', '55%']}
-              h={['40vh', '40vh', '40vh', '80%']}
+              w="100%"
+              h={['40vh', '40vh', '40vh', '100%']}
               mb={['12px', '12px', 0]}
               flexShrink={1}>
               <animated.div
@@ -113,7 +117,7 @@ export default function Page() {
               </animated.div>
             </Box>
           </Flex>
-        </>
+        </Flex>
       )}
     </>
   );
